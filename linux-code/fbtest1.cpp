41e244bc0f0e7e347a4673acc96a43e8e09153ef
Aruth Kandage <aruthk@aruthk-laptop.(none)>
2013-04-26 17:04:45 -0400

    c

--- linux-code/fbtest1.cpp
+++ linux-code/fbtest1.cpp
@@ -1,7 +1,9 @@
 #include <sys/types.h>
-#include <fcntl.h>
-#include <unistd.h>
+#include <sys/ioctl.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <linux/fb.h>
+#include <unistd.h>
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -10,14 +12,25 @@
 #define FB "/dev/fb0"
 
 int main(int argc, char** argv) {
+	int retval;
 	int fd;
 
+	fb_var_screeninfo screen_info;
+	
 	fd = open(FB, O_RDWR);
 	if(fd == -1) {
 		printf("Could not open frame buffer for I/O.\n");
 		exit(1);
 	}
+	
+	retval = ioctl(fd, FBIOGET_VSCREENINFO, &screen_info);
+	if(retval == -1) {
+		printf("Error while querying frame buffer info.\n");
+		exit(1);
+	}
 
+	printf("%dx%d\n", screen_info.xres, screen_info.yres);
+	
 	close(fd);
 	return 0;
 }
